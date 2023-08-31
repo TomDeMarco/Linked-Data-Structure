3428fb4922b600c81f13d7aa9a156cc3939e84a7
TomDeMarco <143742278+TomDeMarco@users.noreply.github.com>
2023-08-31 14:20:35 -0400

    Update EmptySequenceException.java

--- EmptySequenceException.java
+++ EmptySequenceException.java
@@ -1,8 +1,4 @@
 
-/** A runtime exception that signals the Sequence<T> object is empty
- * @author Sherif Khattab
- *
- */
 public class EmptySequenceException extends RuntimeException {
 
 	private static final long serialVersionUID = 575262834263843711L;
